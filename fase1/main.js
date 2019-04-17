130cee6060797397d014bc4afab1db439dad8367
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-17 23:48:35 +0200

    add exercice 1st phase

---
+++ fase1/main.js
@@ -0,0 +1,13 @@
+'use strict';
+/* Fase 1
+Vamos a crear un repo por persona, y vamos a hacer una función que escriba patata 10 veces en la consola la función se llamará ten() */
+
+console.log('prueba');
+
+function ten(){
+for(let i = 0; i < 10; i++){
+    console.log('patata');
+    }
+};
+
+ten();//si no la llamo no se ejecuta
