e9e97476dd6f0be7567f289af565e4f3d25a8328
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-18 23:03:45 +0200

    resolve exercice5 with help

--- fase-5/main.js
+++ fase-5/main.js
@@ -1,2 +1,38 @@
 'use strict'
-console.log('hola');
+
+const myWordList =  [
+    {
+      text: 'Pencil',
+      total: 6
+    },
+    {
+      text: 'Thermo',
+      total: 2
+    },
+    {
+      text: 'TV',
+      total: 8
+    },
+    {
+      text: 'Phone',
+      total: 4
+    }
+  ];
+
+//en esta funcion toma como parametro CUALQUIER ARRAY PARA QUE SE PUEDA UTILIZAR
+//la longitud del array será el límite de vueltas y es menor que i para que se ejecute 4 veces
+//dentro se llama a la funcion writeThis que va a repetir la escritura
+//primera vuelta el texto de la clave objeto tantas veces como ponga en la cifra de la clave total
+  function writeMyArray(arr){
+    for(let i = 0; i < arr.length; i++){
+      writeThis( arr[i].text,arr[i].total);
+    }
+
+  }
+
+  function writeThis(word, times){
+    for(let i = 1; i <= times; i++){//cambio el menor o igual sino se queda uno menos
+        console.log(word);
+    }
+  }
+    writeMyArray(myWordList);
