078a0ecdff0e828fff2554c5656f6c9798ce1c5f
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-18 11:27:50 +0200

    add some info comments

--- fase-4/main.js
+++ fase-4/main.js
@@ -5,7 +5,7 @@ const randomNumber= getRandomNumber(10);//<--
  function getRandomNumber(max) {       //   |
    return Math.ceil(Math.random() * max);// |
  }                          //              |                                
- console.log(randomNumber);//               |
+ console.log(`Este es el numero seleccionado ${randomNumber}`);//               |
 ///Explicacion: la funcion randomNumber tiene como parámetro max y como argumento 10. ¿donde devuelve su resultado? se llama a la función en una constante que va a guardar su dato cajita.
 
 function writeThis(word, times){
